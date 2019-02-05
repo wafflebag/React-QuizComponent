2a7f61f80ad0430eab3e9d9c9237c3ef6cff9903
wafflebag <Cris.Pauly@paulaschoice.com>
2019-02-05 14:13:54 -0800

    section 1 bug fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { quiz_postion: 1 };
+		this.state = { quiz_position: 1 };
 	}
 	render() {
 		return (
