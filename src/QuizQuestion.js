1aeb13b4889bd36bcaf3b4b49597f720b05a5dd1
wafflebag <Cris.Pauly@paulaschoice.com>
2019-02-06 11:44:58 -0800

    working on section 7 wip

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
+	constructor(props) {
+		super(props);
+		this.State = { incorrectAnswer: false };
+	}
 	handleClick(buttonText) {
 		if (buttonText === this.props.quiz_question.answer) {
+			this.setState({ incorrectAnswer: false });
 			this.props.showNextQuestionHandler();
+		} else {
+			this.setState({ incorrectAnswer: true });
 		}
 	}
 	render() {
@@ -26,6 +33,7 @@ class QuizQuestion extends Component {
 						})}
 					</ul>
 				</section>
+				{this.State.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
 			</main>
 		);
 	}
