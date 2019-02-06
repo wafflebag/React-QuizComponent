648477e2174c863d610503d7e08f75c9ad2db252
wafflebag <Cris.Pauly@paulaschoice.com>
2019-02-06 11:11:02 -0800

    section 6 work

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,11 @@ class Quiz extends Component {
 		super(props);
 		this.state = { quiz_position: 1 };
 	}
+	showNextQuestion() {
+		this.setState(state => {
+			return { quiz_position: state.quiz_position + 1 };
+		});
+	}
 	render() {
 		const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 		return (
@@ -16,7 +21,10 @@ class Quiz extends Component {
 				{isQuizEnd ? (
 					<QuizEnd />
 				) : (
-					<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+					<QuizQuestion
+						quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+						showNextQuestionHandler={this.showNextQuestion.bind(this)}
+					/>
 				)}
 				;
 			</div>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
+	handleClick() {}
 	render() {
 		return (
 			<li>
-				<button>{this.props.button_text}</button>
+				<button onClick={this.handleClick.bind(this)}>{this.props.button_text}</button>
 			</li>
 		);
 	}
