df75481d422c1a4932e0ed6525fd53d9f714bf54
wafflebag <Cris.Pauly@paulaschoice.com>
2019-02-06 11:16:44 -0800

    section 6 bug fixes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component {
-	handleClick(buttonText) {}
+	handleClick(buttonText) {
+		if (buttonText === this.props.quiz_question.answer) {
+			this.props.showNextQuestionHandler();
+		}
+	}
 	render() {
 		return (
 			<main>
